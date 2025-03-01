54c74178f28a126a41c05ad3e5c7d1bb7ab0a070
smartmohan20 <dev.smartmohan@gmail.com>
2025-03-02 03:42:18 +0530

    Changes for tawilwind sty;les for black theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#000000',
+        secondary: '#ffffff',
+        accent: '#333333',
+      },
+    },
   },
   plugins: [],
 }
-
